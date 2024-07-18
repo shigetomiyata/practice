5c2cef7e7d8dbf4a4cf22241fe9cf4a4e6c291e4
shigetomiyata <shigetomiyata@gmail.com>
2024-07-18 18:35:40 +0900

    Create TangleAsset.java

---
+++ TangleAsset.java
@@ -0,0 +1,15 @@
+public abstract class TangleAsset{
+    private Sting name;
+    private int name;
+    private String color;
+
+    public TangleAsset(String name, int price, String color){
+        this.name = name;
+        this.price = price;
+        this.color = color;
+　　　　｝
+public String getName(){return this.name;}
+public int getPrice(){return this.price;}
+public String getColor(){return this.color;}
+
+}
